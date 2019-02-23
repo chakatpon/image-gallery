5fe2f2bf78a4d2831f26c63c2eabe3afe768b2fb
Vanoldschool\Van_oldschool <DoctorVan-za@hotmail.com>
2019-02-23 22:52:55 +0700

    finish the color game

--- image-gallery/colorGame.js
+++ image-gallery/colorGame.js
@@ -1,55 +1,75 @@
-var colors = generateRandomColors(6);
+var game = {};
 
+var numSquare = 6;
+var colors = generateRandomColors(numSquare);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#messageDisplay");
 var header1 = document.querySelector(".header1");
-var reset = document.querySelector("#reset");
-var easyBtn = document.querySelector("#easyBtn");
-var hardBtn = document.querySelector("hardBtn");
+var resetBtn = document.querySelector("#reset");
+var modeBtns = document.querySelectorAll(".mode");
 
-colorDisplay.textContent = pickedColor.toUpperCase();
+game.init = function(){
+    setupModeButton();
+    setupSquares();
+    reset();
+}
 
-reset.addEventListener("click", function(){
+game.init();
 
-    colors = generateRandomColors(6);
 
-    pickedColor = pickColor();
 
-    colorDisplay.textContent = pickedColor.toUpperCase();
 
-    header1.style.backgroundColor = "#232323";
-    reset.textContent = "New Color";
 
-    for(var i = 0; i < squares.length; i++){
-        squares[i].style.backgroundColor = colors[i];
-    }
 
+
+
+
+resetBtn.addEventListener("click", function(){
+    reset();
 });
 
-for(var i = 0; i < squares.length; i++){
-    //add initial color to square.
-    squares[i].style.backgroundColor = colors[i];
-    //add click listener to square.
-    squares[i].addEventListener("click", function(){
-        
 
-        var clickedColor = this.style.backgroundColor;
-
-        if(clickedColor === pickedColor){
-            messageDisplay.textContent = "Correct!";
-            changeColor(clickedColor);
-            reset.textContent = "Play Again ?";
-            header1.style.backgroundColor = clickedColor;
-        }else {
-            this.style.backgroundColor = "#232323";
-            messageDisplay.textContent = "Try Again.";
-
-        }
-    });
+function setupModeButton(){
+    for(var i = 0; i < modeBtns.length; i++){
+        modeBtns[i].addEventListener("click", function(){
+            for(var j = 0; j < modeBtns.length; j++){
+                modeBtns[j].classList.remove("selected");
+            }
+    
+            this.classList.add("selected");
+            this.textContent === "EASY" ? numSquare = 3 : numSquare = 6;
+            reset();
+        });
+    }
 
 }
+
+function setupSquares(){
+    for(var i = 0; i < squares.length; i++){
+        //add initial color to square.
+        squares[i].style.backgroundColor = colors[i];
+        //add click listener to square.
+        squares[i].addEventListener("click", function(){
+            
+    
+            var clickedColor = this.style.backgroundColor;
+    
+            if(clickedColor === pickedColor){
+                messageDisplay.textContent = "Correct !";
+                changeColor(clickedColor);
+                reset.textContent = "PLAY AGAIN ?";
+                header1.style.backgroundColor = clickedColor;
+            }else {
+                this.style.backgroundColor = "#232323";
+                messageDisplay.textContent = "Try Again.";
+    
+            }
+        });
+    
+    }
+};
 
 function changeColor(color){
     for(var i = 0; i < squares.length; i++){
@@ -76,4 +96,27 @@ function generateRandomColors(length){
 
     return arrs;
     
+}
+
+function reset(){
+
+    colors = generateRandomColors(numSquare);
+
+    pickedColor = pickColor();
+
+    colorDisplay.textContent = pickedColor.toUpperCase();
+
+    header1.style.backgroundColor = "steelblue";
+    reset.textContent = "NEW COLOR";
+    messageDisplay.textContent = "";
+
+    for(var i = 0; i < squares.length; i++){
+        if(colors[i]){
+            squares[i].style.display = "block";
+            squares[i].style.backgroundColor = colors[i];           
+        }else{
+            squares[i].style.display = "none";
+        }
+        
+    }
 }
