5e6df5161793da435170ed53f78c99681b0d200f
chakatpon.k <doctorVan-za@hotmail.com>
2019-02-25 18:05:29 +0700

    create todolist application

--- image-gallery/todoListApp/assets/js/todos.js
+++ image-gallery/todoListApp/assets/js/todos.js
@@ -1,1 +1,28 @@
-alert("todos.js is connected.");
+
+// Check off specific todos by clicking.
+$("ul").on("click","li",function(){
+    $(this).toggleClass("completed");
+});
+
+// click on x to remove a tode.
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+    
+    
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text'").fadeToggle();
+
+});
