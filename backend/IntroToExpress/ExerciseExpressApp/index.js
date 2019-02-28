40a7ec6295d8263f0012bbdc931297c87cb81493
chakatpon.k <doctorVan-za@hotmail.com>
2019-02-28 17:34:42 +0700

    change Project structure: create frontend and backend directory.

---
+++ backend/IntroToExpress/ExerciseExpressApp/index.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var app = express();
+
+
+
+app.get("/", function(req, res){
+    res.send("Hi !!!");
+});
+
+app.get("/speak/:animal/", function(req, res){
+    var pet = req.params.animal;
+    if(pet === "pig"){
+        res.send("The " + pet + " says 'Oink'");
+    }else if(pet === "cow"){
+        res.send("The " + pet + " says 'Moo'");
+    }else if(pet === "dog"){
+        res.send("The " + pet + " says 'Woof Woof!'");
+    }
+});
+
+app.get("/repeat/:greeting/:num/", function(req, res){
+    var greet = req.params.greeting;
+    var number = Number(req.params.num);
+    var result = "";
+    for(var i = 0; i < number; i++ ){
+        result += greet + " ";
+    }
+    res.send(result);
+})
+
+app.get("*", function(req, res){
+    res.send("sorry, page not found...What are you doing with your life?");
+})
+
+app.listen(3000,function(){
+    console.log("SERVER IS LISTENING ON PORT: 3000");
+})
---
+++ backend/IntroToNode/objectives.md
@@ -0,0 +1,5 @@
+#Introduction to Express
+
+*What is a framework? How is it dirrerent from a library?
+*What is Express?
+*Why are we using Express?
