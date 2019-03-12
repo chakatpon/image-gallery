4cdc5cd93dcaff131a32d16eacf62966b9956a16
Vanoldschool\Van_oldschool <DoctorVan-za@hotmail.com>
2019-03-13 01:02:35 +0700

    create youtube api application not done yet

---
+++ reactApp/videos/src/component/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
+import VideoList from './VideoList';
+
+
+class App extends React.Component {
+
+    state = { videos: [], selectedVideo: null};
+
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
+            params:{
+                q: term
+            }
+        })
+
+        console.log(response);
+        this.setState({ videos: response.data.items});
+
+    }
+
+    onVideoSelect = (video) => {
+        console.log('From the App!', video);
+    }
+
+    render() {
+        return (
+            <div className='ui container'>
+                <SearchBar onFormSubmit={this.onTermSubmit}/>
+                I have {this.state.videos.length} videos.
+                <VideoList onVideoSelecct={this.onVideoSelect} videos={this.state.videos}/>
+            </div>
+        );
+    }
+}
+
+export default App;
