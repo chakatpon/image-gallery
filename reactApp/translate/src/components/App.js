85d3856e09c266a330c5ab152d9ae71199cc1e89
chakatpon.k <doctorVan-za@hotmail.com>
2019-03-27 18:41:20 +0700

    context practice

---
+++ reactApp/translate/src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+
+
+class App extends React.Component {
+    state = { language: 'english' };
+
+    onLanguageChange = (language) => {
+        this.setState({ language })
+    }
+
+    render() {
+        return (
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i onClick={() => this.onLanguageChange('english')} className="flag us" />
+                    <i onClick={() => this.onLanguageChange('dutch')} className="flag nl" />
+                </div>
+                {this.state.language}
+                <LanguageContext.Provider value={this.state.language}>
+                <UserCreate/>
+                </LanguageContext.Provider>
+            </div>
+        )
+    }
+}
+
+export default App;
